901016c9ec219982f381ba011711a20a6cff526c
Ajat Darojat <ajatdarojat45@gmail.com>
2022-04-20 14:18:48 +0700

    wiring chat list

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,4 +1,9 @@
-export default function ChatList({ setIsFriendsList, isFriendsList, user }) {
+export default function ChatList({
+	setIsFriendsList,
+	isFriendsList,
+	user,
+	rooms,
+}) {
 	return (
 		<div className="side-one">
 			<div className="row heading">
@@ -34,23 +39,27 @@ export default function ChatList({ setIsFriendsList, isFriendsList, user }) {
 			</div>
 
 			<div className="row sideBar">
-				<div className="row sideBar-body">
-					<div className="col-sm-3 col-xs-3 sideBar-avatar">
-						<div className="avatar-icon">
-							<img src="https://bootdey.com/img/Content/avatar/avatar1.png" />
-						</div>
-					</div>
-					<div className="col-sm-9 col-xs-9 sideBar-main">
-						<div className="row">
-							<div className="col-sm-8 col-xs-8 sideBar-name">
-								<span className="name-meta">John Doe </span>
+				{rooms.map((room) => {
+					return (
+						<div className="row sideBar-body" key={room.$id}>
+							<div className="col-sm-3 col-xs-3 sideBar-avatar">
+								<div className="avatar-icon">
+									<img src="https://bootdey.com/img/Content/avatar/avatar1.png" />
+								</div>
 							</div>
-							<div className="col-sm-4 col-xs-4 pull-right sideBar-time">
-								<span className="time-meta pull-right">18:18 </span>
+							<div className="col-sm-9 col-xs-9 sideBar-main">
+								<div className="row">
+									<div className="col-sm-8 col-xs-8 sideBar-name">
+										<span className="name-meta">John Doe </span>
+									</div>
+									<div className="col-sm-4 col-xs-4 pull-right sideBar-time">
+										<span className="time-meta pull-right">18:18 </span>
+									</div>
+								</div>
 							</div>
 						</div>
-					</div>
-				</div>
+					);
+				})}
 			</div>
 		</div>
 	);
--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -4,12 +4,14 @@ import { useState } from "react";
 import { useEffect } from "react";
 import appwrite from "../sdk/appwrite";
 import ChatRoom from "../components/ChatRoom";
+import { Query } from "appwrite";
 
 export default function Chat() {
 	const [isFriendsList, setIsFriendsList] = useState(false);
 	const [users, setUsers] = useState([]);
 	const [user, setUser] = useState(null);
 	const [receiver, setReceiver] = useState(null);
+	const [rooms, setRooms] = useState([]);
 
 	useEffect(() => {
 		fetch("http://localhost:4000/users")
@@ -31,6 +33,70 @@ export default function Chat() {
 		);
 	}, []);
 
+	useEffect(() => {
+		if (user) {
+			let promise = appwrite.database.listDocuments("625f2658dbe1156b814e", [
+				Query.search("userIds", [user?.$id]),
+			]);
+			promise.then(
+				function(response) {
+					setRooms(response.documents);
+				},
+				function(error) {
+					console.log(error); // Failure
+				}
+			);
+		}
+	}, [user]);
+
+	useEffect(() => {
+		let promise = appwrite.database.listDocuments("625f2658dbe1156b814e", [
+			Query.search("userIds", [user?.$id]),
+		]);
+		promise.then(
+			function(response) {
+				setRooms(response.documents);
+			},
+			function(error) {
+				console.log(error); // Failure
+			}
+		);
+	}, [receiver]);
+
+	useEffect(() => {
+		if (receiver) {
+			let promise = appwrite.database.listDocuments("625f2658dbe1156b814e", [
+				Query.search("userIds", [user?.$id]),
+				Query.search("userIds", [receiver?.$id]),
+			]);
+			promise.then(
+				function(response) {
+					if (response.documents.length < 1) {
+						let promise = appwrite.database.createDocument(
+							"625f2658dbe1156b814e",
+							"unique()",
+							{
+								name: "",
+								userIds: [user?.$id, receiver?.$id],
+							}
+						);
+						promise.then(
+							function(response) {
+								console.log(response); // Success
+							},
+							function(error) {
+								console.log(error); // Failure
+							}
+						);
+					}
+				},
+				function(error) {
+					console.log(error); // Failure
+				}
+			);
+		}
+	}, [receiver]);
+
 	return (
 		<div className="container app">
 			<div className="row app-one">
@@ -39,6 +105,7 @@ export default function Chat() {
 						isFriendsList={isFriendsList}
 						setIsFriendsList={setIsFriendsList}
 						user={user}
+						rooms={rooms}
 					/>
 					<FriendsList
 						isFriendsList={isFriendsList}
